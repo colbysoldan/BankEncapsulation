d240407267ff2a4021f862c45543567660cd6600
Colby Soldan <colbylsoldan@gmail.com>
2022-06-13 15:01:25 -0500

    first commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("Deposit amount:");
+            var couldParse = double.TryParse(Console.ReadLine(), out double amount);
+
+            while (!couldParse)
+            {
+                Console.Write("Please enter a valid amount: ");
+                couldParse = double.TryParse(Console.ReadLine(), out amount);
+            }
+
+            account.Deposit(amount);
+
+            Console.WriteLine($"Your new balance is ${account.GetBalance()}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
